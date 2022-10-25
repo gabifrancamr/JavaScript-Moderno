b6287d99074522186f01a641cb33c1339e0cca7e
Gabriela França <gabrielafranca2008@hotmail.com>
2022-10-25 16:05:18 -0300

    tipos de export

---
+++ 09-tipos-de-export-esm/inline.js
@@ -0,0 +1,9 @@
+export function inline(){
+    console.log('Export nomeado inline')
+}
+
+export default function defaultInline(){
+    console.log('Export default inline')
+}
+
+//só pode ter um export default por arquivo
